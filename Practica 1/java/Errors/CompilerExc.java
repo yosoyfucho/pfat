899334ee0d6c6cf84dc46cce483d95d87c952225
Fucho <fucho@me.com>
2015-02-20 18:25:40 +0100

    pequeños commits
    aún quedan más por venir

--- Practica 1/java/Errors/CompilerExc.java
+++ Practica 1/java/Errors/CompilerExc.java
@@ -2,5 +2,13 @@ package Errors;
 
 public class CompilerExc extends Exception
 {
-	
+	public CompilerExc()
+	{
+		System.out.println("Error");
+	}
+
+	public CompilerExc (String message)
+	{
+		System.out.println(message);
+	}
 }
