7375bbe3bbfdea6c489c15e55c173b3dd1634642
Fucho <fucho@me.com>
2015-02-24 15:45:32 +0100

    A Luis lo que es de Luis

--- Practica 1/java/AST/Initialization.java
+++ Practica 1/java/AST/Initialization.java
@@ -3,12 +3,12 @@ package AST;
 import Errors.*;
 import java.io.*;
 
-public class Initialitation
+public class Initialization
 {
 	public Ident i;
 	public SimpleStatementList ssl;
 
-	public Initialitation (Ident i, SimpleStatementList ssl)
+	public Initialization (Ident i, SimpleStatementList ssl)
 	{
 		this.i = i;
 		this.ssl = ssl;
