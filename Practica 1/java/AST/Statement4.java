42d4e5f6c42685025384fd633050833647aa2acc
Fucho <fucho@me.com>
2015-03-09 10:04:17 +0100

    Estado 1 error

--- Practica 1/java/AST/Statement4.java
+++ Practica 1/java/AST/Statement4.java
@@ -4,8 +4,8 @@ import Errors.*;
 public class Statement4 implements Statement{
 	public ExpLog el;
 	public Statement s;
-	
-	public Statement4 (Statement s, ExpLog el){
+
+	public Statement4 (ExpLog el, Statement s){
 		this.s = s;
 		this.el = el;
 	}
