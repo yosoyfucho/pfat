3cbf7169a247002f6aecac13830a0a70f4544566
Fucho <fucho@me.com>
2015-03-10 19:17:13 +0100

    compile incluye los ejemplos 1-8 y los errores sintacticos

--- Practica 1/compile.sh
+++ Practica 1/compile.sh
@@ -16,4 +16,39 @@ java -cp ../../../ JLex.Main Yylex
 echo -e "\n [compile.sh] compilando Main.java ... \n"
 cd ..
 javac -d ../class -cp ../../CUP:../class:. Main.java
-
+echo -e "\ [compile.sh] probando ejemplos ... \n\n"
+cd java
+echo -e "\ [compile.sh] probando ejemplo 1 ... \n"
+java -cp ../../CUP:../class Main ./ejemplos/Ejem1/Ejem1.stm
+echo "\n"
+echo -e "\ [compile.sh] probando ejemplo 2 ... \n"
+java -cp ../../CUP:../class Main ./ejemplos/Ejem2/Ejem2.stm
+echo "\n"
+echo -e "\ [compile.sh] probando ejemplo 3 ... \n"
+java -cp ../../CUP:../class Main ./ejemplos/Ejem3/Ejem3.stm
+echo "\n"
+echo -e "\ [compile.sh] probando ejemplo 4 ... \n"
+java -cp ../../CUP:../class Main ./ejemplos/Ejem4/Ejem4.stm
+echo "\n"
+echo -e "\ [compile.sh] probando ejemplo 5 ... \n"
+java -cp ../../CUP:../class Main ./ejemplos/Ejem5/Ejem5.stm
+echo "\n"
+echo -e "\ [compile.sh] probando ejemplo 6 ... \n"
+java -cp ../../CUP:../class Main ./ejemplos/Ejem6/Ejem6.stm
+echo "\n"
+echo -e "\ [compile.sh] probando ejemplo 7 ... \n"
+java -cp ../../CUP:../class Main ./ejemplos/Ejem7/Ejem7.stm
+echo "\n"
+echo -e "\ [compile.sh] probando ejemplo 8 ... \n"
+java -cp ../../CUP:../class Main ./ejemplos/Ejem8/Ejem8.stm
+echo "\n\n"
+echo -e "\ [compile.sh] probando Errores Sintacticos ... \n\n"
+echo -e "\ [compile.sh] probando Error Sintactico 1 ... \n"
+java -cp ../../CUP:../class Main ./ejemplos/ErrSint1/errSint1.stm
+echo "\n Es normal la captura de excepcion\n\n"
+echo -e "\ [compile.sh] probando Error Sintactico 2 ... \n"
+java -cp ../../CUP:../class Main ./ejemplos/ErrSint2/errSint2.stm
+echo "\n Es normal la captura de excepion\n\n"
+echo -e "\ [compile.sh] probando Error Sintactico 3 ... \n"
+java -cp ../../CUP:../class Main ./ejemplos/ErrSint3/errSint3.stm
+echo "\n Es normal la captura de excepion\n\n"
