917bcf5fe5c7931e2a71b94d2d2278abc73eb3d3
albmoren <100282739@alumnos.uc3m.es>
2015-03-10 15:27:22 +0100

    Modificaciones parser InitialState & ExpLog, clase java AST/Bool2

--- Practica 1/java/AST/Bool2.java
+++ Practica 1/java/AST/Bool2.java
@@ -4,11 +4,11 @@ import Errors.*;
 public class Bool2 implements ExpLog {
 	public ExpLog e1;
 	public ExpLog e2;
-	public boolean op;
+	//public boolean op;
 	
-	public Bool2 (ExpLog e1, ExpLog e2, boolean op) {
+	//public Bool2 (ExpLog e1, ExpLog e2, boolean op) {
+	public Bool2 (ExpLog e1, ExpLog e2) {
 		this.e1 = e1;
 		this.e2 = e2;
-		this.op = op;
 	}
 }
