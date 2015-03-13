4c6b2bfcce7b85560453ea6aed9b2737453af7c8
albmoren <100282739@alumnos.uc3m.es>
2015-03-13 11:16:37 +0100

    add TableEntry.java SymbolTable.java

---
+++ Practica_2/java/Compiler/SymbolTable.java
@@ -0,0 +1,70 @@
+package Compiler;
+import java.util.Vector;
+import Errors.*;
+
+
+public class SymbolTable()
+{
+
+	private static Vector<TableEntry> table = new Vector<TableEntry>();
+	
+	public static void newEntry(String name, String type) throws DoubleDefExc
+	{
+
+		TableEntry myTable = new SymbolTable.search(name, type);
+		if (myTable == NULL)
+		{
+			myTable = new TableEntry(name);
+			table.add(myTable);
+		}
+		else
+		{
+			throw new DoubleDefExc(name);
+		}
+
+
+	}
+
+	public static TableEntry search(String name, String type)
+	{
+		TableEntry searchEntry = NULL;
+
+		int i=0;
+		while (i < table.size())
+		{
+			searchEntry = table.get(i);
+			// Busca una variable con mismo nombre declarada
+			if (searchEntry.getName().equals(name))
+			{ 
+				if (searchEntry.getType().equals(type))
+				{
+					break;
+				}
+				else
+				{
+					if (searchEntry.getType().equals("status"))
+					{
+
+					}
+					if (searchEntry.getType().equals("final state"))
+					{
+
+					}
+					if (searchEntry.getType().equals("inputs"))	
+					{
+
+					}
+					if (searchEntry.getType().equals("outputs"))
+					{
+	
+					}
+				}
+			}
+			searchEntry = NULL;
+			i++;
+		}
+
+		return searchEntry;
+	}
+
+}
---
+++ Practica_2/java/Compiler/TableEntry.java
@@ -0,0 +1,24 @@
+package Compiler;
+
+public class TableEntry {
+
+	private String name;
+	privete String type;
+
+	public TableEntry (String name, String type)
+	{
+		this.name = name;
+		this.type = type;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public String getType()
+	{
+		return name;
+	}
+
+}
