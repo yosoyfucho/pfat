a078511a5a501e8dadb282ceb3dea2de67a1ca7b
albmoren <100282739@alumnos.uc3m.es>
2015-03-27 00:04:43 +0100

    ultimo commit previo al merge

--- Practica_1/compile.sh
+++ Practica_1/compile.sh
@@ -1,23 +1,41 @@
 # Script compilar/ejecutar Practica_1
 
+clear
+
 echo -e "\n [compile.sh] Compilando package Errors/ ... \n"
 javac -d class/ java/Errors/*.java
+echo ""
+read -n 1 -s -p ">>> Pulsa una tecla..."
+
+clear
 
 echo -e "\n [compile.sh] Compilando package AST/ ... \n"
 javac -d class/ -cp java/ java/AST/*.java
+echo ""
+read -n 1 -s -p ">>> Pulsa una tecla..."
+
+clear
 
 echo -e "\n [compile.sh] Ejecutando java_cup.Main parser ... \n"
 cd java/Parser
 java -cp ../../../CUP/ java_cup.Main parser
-cd ../..
+echo ""
+read -n 1 -s -p ">>> Pulsa una tecla..."
 
+clear
+
+cd ../..
 echo -e "\n [compile.sh] Ejecutando JLex.Main Yylex ... \n"
 cd java/Lexer
 java -cp ../../../ JLex.Main Yylex
-cd ../..
+echo ""
+read -n 1 -s -p ">>> Pulsa una tecla..."
 
+clear
+
+cd ../..
 echo -e "\n [compile.sh] Compilando Main.java ... \n"
 # Genera los .class de java/Parser | java/Lexer | Main.java
 javac -d class/ -cp ../CUP/:java java/Main.java
 
-# java -cp class/:../CUP/ Main ../Ejemplos/Ejem1/ejem1.stm
+clear
---
+++ Practica_1/test_examples.sh
@@ -0,0 +1,38 @@
+# script ejecucion ejemplos Practica 1 - PFAT
+clear
+
+echo -e "\n--- Pruebas Practica 1 ---\n"
+
+for ((i=1; i<=10; i++))
+do
+	echo -e "\n>>> Ejecutando ejem$i.stm ... \n"
+	java -cp class/:../CUP/ Main ../Ejemplos/Ejem$i/ejem$i.stm
+	echo ""
+	read -n 1 -s -p ">>> Pulsa una tecla..."
+	echo -e "\n\n---"
+done
+
+clear 
+
+echo -e "\n--- Pruebas Errores Sintacticos ---\n"
+
+echo -e "\n>>> Ejecutando errLex1.stm ... \n"
+java -cp class/:../CUP/ Main ../Ejemplos/ErrLex1/errLex1.stm
+echo ""
+read -n 1 -s -p ">>> Pulsa una tecla..."
+echo -e "\n\n---"
+
+clear 
+
+echo -e "\n--- Pruebas Errores Sintacticos ---\n"
+
+for ((i=1; i<=3; i++))
+do
+	echo -e "\n>>> Ejecutando errSint$i.stm ... \n"
+	java -cp class/:../CUP/ Main ../Ejemplos/ErrSint$i/errSint$i.stm
+	echo ""
+	read -n 1 -s -p ">>> Pulsa una tecla..."
+	echo -e "\n\n---"
+done
+
+clear
