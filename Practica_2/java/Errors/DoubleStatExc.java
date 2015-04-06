1fc576f0d6e178cf4a54a3a4cd70c02daf4c69b5
Fucho <fucho@me.com>
2015-04-06 22:13:59 +0200

    Añadido boolean a TableEntry y nueva Excepción para la doble declaración de asignación

---
+++ Practica_2/java/Errors/DoubleStatExc.java
@@ -0,0 +1,14 @@
+package Errors;
+
+public class DoubleStatExc extends CompilerExc
+{
+	public DoubleStatExc()
+	{
+		System.out.println("Error: La asignaci");
+	}
+
+	public DoubleStatExc (String message)
+	{
+		System.out.println(message);
+	}
+}
