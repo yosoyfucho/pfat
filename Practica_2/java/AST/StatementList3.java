72ba92a1a0fe12f09bccf983ddf5028243e47f43
Fucho <fucho@me.com>
2015-04-13 12:58:41 +0200

    fallos varios en ejemplos y 14 especificar posición STOP

---
+++ Practica_2/java/AST/StatementList3.java
@@ -0,0 +1,16 @@
+package AST;
+import Compiler.*;
+import Errors.*;
+
+/*
+StatementList ::= STOP PC {:RESULT = new StatementList3(); :};
+*/
+
+public class StatementList3 implements StatementList
+{
+
+	public void computeType() throws CompilerExc
+	{
+
+	}
+}
---
+++ Practica_2/java/Errors/StopExcep.java
@@ -0,0 +1,10 @@
+package Errors;
+
+public class StopExcep extends CompilerExc
+{
+	public StopExcep()
+	{
+		System.out.println("Error: STOP mal situado");
+	}
+
+}
