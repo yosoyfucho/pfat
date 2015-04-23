6adccdb6ef218ca643541770124cfe23eafdb3d9
Alberto <albmoren@localhost.site>
2015-04-23 15:48:12 +0200

    fichero prueba.java

---
+++ Practica_2/java/GenerateCodeLib/Resultado.java
@@ -0,0 +1,14 @@
+package GeneratedCodeLib;
+
+public class Resultado {
+  public final String var;
+  public final boolean valor;
+  public final int tiempo;
+
+  public Resultado(String v, boolean val, int t) {
+    var=v;
+    valor=val;
+    tiempo=t;
+  }
+}
+
---
+++ Practica_2/prueba.java
@@ -0,0 +1,102 @@
+package GenerateCodeLib;
+
+public class Prueba
+{
+	
+	public static void main()
+	{
+
+		public String s1 = "s1";
+		public String s2 = "s2";
+		public String s3 = "s3";
+		public String s4 = "s4";
+
+		public String a = "a";
+		public String b = "b";
+		public String c = "c";
+
+		public boolean sal;
+
+		public String currentState = s1;
+		public String finalState = s4;
+
+		sal = false;
+
+		BuffererReader r = new BuffererReader();
+		Vector<String> entradas = new Vector<String>;
+		
+		entradas = obtener(BufferedReader br);
+
+		BufferedWriter w = new BufferedWriter();
+
+		Salida output = new Salida(w);
+
+		int nEventos = entradas.size();
+
+		for (String inputEvent : entradas)
+		{
+			if (currentState.equals(s1) && inputEvent.equals(a))
+			{
+				currentState = s2;
+				output.insertaResultado(currentState, sal, nEventos-1);
+				if (currentState.equals(finalState))
+				{
+					 output.generarResultado();
+				}
+			}
+
+			if (currentState.equals(s2) && inputEvent.equals(c)) 
+			{
+				currentState = s1;
+				output.insertaResultado(currentState, sal, nEventos-1);
+				if (currentState.equals(finalState))
+				{
+					output.generarResultado();
+				}
+			}
+
+			if (currentState.equals(s2) && inputEvent.equals(b)) 
+			{
+				currentState = s3;
+				output.insertaResultado(currentState, sal, nEventos-1);
+				if (currentState.equals(finalState))
+				{
+					output.generarResultado();
+				}
+			}
+
+			if (currentState.equals(s3) && inputEvent.equals(a)) 
+			{
+				currentState = s2;
+				output.insertaResultado(currentState, sal, nEventos-1);
+				if (currentState.equals(finalState))
+				{
+					output.generarResultado();
+				}
+			}
+
+			if (currentState.equals(s3) && inputEvent.equals(b)) 
+			{
+				currentState = s1;
+				output.insertaResultado(currentState, sal, nEventos-1);
+				if (currentState.equals(finalState))
+				{
+					output.generarResultado();
+				}
+			}
+
+			if (currentState.equals(s3) && inputEvent.equals(c)) 
+			{
+				currentState = s4;
+				sal = true;
+				output.insertaResultado(currentState, sal, nEventos-1);
+				if (currentState.equals(finalState))
+				{
+					output.generarResultado();
+				}
+			}
+
+			nEventos--;
+		}
+	}
+}
