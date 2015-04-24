504d10a2c572d5b4acbe05a8216edeff05a25012
Fucho <fucho@me.com>
2015-04-24 13:33:37 +0200

    Distingue si el string es un path con fich.java o directamente fich.java

--- Practica_2/PruebaSplit.java
+++ Practica_2/PruebaSplit.java
@@ -4,10 +4,21 @@ public class PruebaSplit
 {
   public static void main (String args [])
   {
-    String [] aux = args[0].split("/");
-    int auxiliar = aux.length - 1;
-    String aux2 = aux[auxiliar];
-    String [] aux3 = aux2.split("\\.");
-    System.out.println("me quedo con '" +aux3[0]+"'");
+    System.out.println("He leido :"+args[0]+"\n");
+
+    if (args[0].contains("/"))
+    {
+      String [] aux = args[0].split("/");
+      int auxiliar = aux.length - 1;
+      String aux2 = aux[auxiliar];
+      String [] aux3 = aux2.split("\\.");
+      System.out.println("me quedo con ->'" +aux3[0]+"'");
+    }
+    else
+    {
+      String [] aux4 = args[0].split("\\.");
+      System.out.println("me quedo con ->'"+aux4[0]+"'");
+    }
+
   }
 }
